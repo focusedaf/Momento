cf4c8d83796ba6bb0d3dedc9076a8ce6028da303
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-07-21 03:12:07 +0530

    stuff

--- src/components/ui/polaroid.jsx
+++ src/components/ui/polaroid.jsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import {
   DraggableCardBody,
   DraggableCardContainer,
 } from "@/components/ui/draggable-card";
 
 export default function Polaroid({ items }) {
-  
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <DraggableCardContainer className="relative flex min-h-screen w-full items-center justify-center overflow-clip">
       <p className="absolute top-1/2 mx-auto max-w-sm -translate-y-3/4 text-center text-2xl font-black text-neutral-400 md:text-4xl dark:text-neutral-800">
@@ -14,19 +15,21 @@ export default function Polaroid({ items }) {
       {items.map((item, index) => (
         <DraggableCardBody
           key={index}
-          className={item.className} 
+          className={item.className}
           onMouseEnter={() => setIsFocused(true)}
           onMouseLeave={() => setIsFocused(false)}
           draggable
         >
-          <img
-            src={item.image}
-            alt={item.title}
-            className="pointer-events-none relative z-10 h-80 w-80 object-cover"
-          />
-          <h3 className="mt-4 text-center text-2xl font-bold text-neutral-700 dark:text-neutral-300">
-            {item.title}
-          </h3>
+          <div className="bg-white dark:bg-neutral-200 p-4 shadow-xl rounded-sm">
+            <img
+              src={item.image}
+              alt={item.title}
+              className="pointer-events-none relative z-10 h-64 w-64 object-cover mb-4"
+            />
+            <h3 className="text-center text-sm font-bold text-neutral-700 dark:text-neutral-800">
+              {item.title}
+            </h3>
+          </div>
         </DraggableCardBody>
       ))}
     </DraggableCardContainer>
