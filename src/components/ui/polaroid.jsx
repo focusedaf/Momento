da89c4f9726b8ccfd4e8540cd01a900915997d06
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-07-21 05:02:56 +0530

    stuff

--- src/components/ui/polaroid.jsx
+++ src/components/ui/polaroid.jsx
@@ -3,23 +3,23 @@ import {
   DraggableCardContainer,
 } from "@/components/ui/draggable-card";
 
-export default function Polaroid({ items }) {
+export default function Polaroid({items,message}) {
   return (
     <DraggableCardContainer className="relative flex min-h-screen w-full items-center justify-center overflow-clip">
       <p className="absolute top-1/2 mx-auto max-w-sm -translate-y-3/4 text-center text-2xl font-black text-neutral-400 md:text-4xl dark:text-neutral-800">
-        If it's your first day at Fight Club, you have to fight.
+        {message}
       </p>
       {items.map((item, index) => (
         <DraggableCardBody key={index} className={item.className}>
-          <div className="bg-white dark:bg-neutral-200 p-4 shadow-xl rounded-sm">
+          <div className="bg-white shadow-xl rounded-md p-4 flex flex-col items-center space-y-3 dark:bg-neutral-200">
             <img
               src={item.image}
               alt={item.title}
-              className="pointer-events-none relative z-10 h-64 w-64 object-cover mb-4"
+              className="relative z-10 h-64 w-64 object-cover mb-4"
             />
-            <h3 className="text-center text-sm font-bold text-neutral-700 dark:text-neutral-800">
+            <p className="text-sm font-semibold text-neutral-700 dark:text-neutral-800 text-center">
               {item.title}
-            </h3>
+            </p>
           </div>
         </DraggableCardBody>
       ))}
--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -14,21 +14,19 @@ const generatePolaroids = (yearIndex) => {
   ];
 
   const zIndexes = [
-    "z-[7]", 
+    "z-[7]",
     "z-[6]",
     "z-[5]",
     "z-[4]",
     "z-[3]",
     "z-[2]",
-    "z-[1]", 
+    "z-[1]",
   ];
-
+  const year = 2006 + yearIndex;
   return Array.from({ length: 7 }, (_, j) => ({
-    title: `Image ${j + 1} - ${2006 + yearIndex}`,
-    image: `https://source.unsplash.com/random/400x400?sig=${
-      yearIndex * 10 + j
-    }`,
-    className: `${positions[j]} ${zIndexes[j]}`, 
+    title:"hello",
+    image: `/timeline/${year}/img${j + 1}.jpg`,
+    className: `${positions[j]} ${zIndexes[j]} object-cover`,
   }));
 };
 
@@ -76,19 +74,20 @@ export const Timeline = ({ data }) => {
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500">
-                {item.year}
-              </h3>
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
               <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
                 {item.year}
               </h3>
-              <Polaroid items={generatePolaroids(index)} />
+              <Polaroid
+                items={generatePolaroids(index)}
+                message={item.message}
+              />
             </div>
           </div>
         ))}
+
         <div
           style={{
             height: height + "px",
